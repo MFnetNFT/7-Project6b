69d458b5ac29ea0946676aead63fc82f8805632e
MFnetNFT <mfnet@live.com>
2023-03-28 13:39:37 -0300

    20230328:13:39

--- truffle-config.js
+++ truffle-config.js
@@ -38,6 +38,10 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
+  dashboard: {
+    port: 24012,
+  },
+  
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
